f4ce7db1858c6d348ed5e928a1393ffdb64a7ed7
Curve <fynnbwdt@gmail.com>
2023-10-11 16:55:53 +0200

    feat: add basic tests

---
+++ tests/node/api.test.js
@@ -0,0 +1,9 @@
+const venmic = require("../../lib");
+const assert = require("assert");
+
+assert(Array.isArray(venmic.list()));
+
+assert.throws(() => venmic.link(10));
+assert.throws(() => venmic.link("", ""));
+
+assert.doesNotThrow(() => venmic.link("Firefox"));
