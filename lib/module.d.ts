21d74433ec519b5c27df20f70dfd7169795ae691
Curve <fynnbwdt@gmail.com>
2023-10-21 21:16:10 +0200

    feat(types): use literalunion in `list`

--- lib/module.d.ts
+++ lib/module.d.ts
@@ -1,10 +1,15 @@
 type DefaultProps = 'node.name' | 'application.name';
 
+type LiteralUnion<
+	LiteralType,
+	BaseType extends string,
+> = LiteralType | (BaseType & Record<never, never>);
+
 export class PatchBay
 {
     unlink(): void;
     
-    list<T extends string = DefaultProps>(props?: T[]): Record<T, string>[];
+    list<T extends string = DefaultProps>(props?: T[]): Record<LiteralUnion<T, string>, string>[];
     link(data: {key: string, value: string, mode: "include" | "exclude"}): boolean;
 
     static hasPipeWire(): boolean;
