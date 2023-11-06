3e24ed1bce3824a63376adb3c070bff8fc6d7aef
Curve <fynnbwdt@gmail.com>
2023-11-06 16:24:08 +0100

    refactor: update tests

--- tests/node/api.test.js
+++ tests/node/api.test.js
@@ -1,22 +1,11 @@
 const venmic = require("../../lib");
 const assert = require("assert");
 
+let patchbay = null;
+
 try
 {
-    const patchbay = new venmic.PatchBay();
-
-    assert(Array.isArray(patchbay.list()));
-    assert(Array.isArray(patchbay.list(["node.name"])));
-
-    assert.throws(() => patchbay.list({}), /expected array/ig);
-    assert.throws(() => patchbay.list([10]), /expected item to be string/ig);
-
-    assert.throws(() => patchbay.link(10), /expected link object/ig);
-    assert.throws(() => patchbay.link({ a: "A", b: "B", c: "C" }), /expected keys/ig);
-    assert.throws(() => patchbay.link({ key: "node.name", value: "Firefox", mode: "gibberish" }), /expected mode/ig);
-
-    assert.doesNotThrow(() => patchbay.link({ key: "node.name", value: "Firefox", mode: "include" }));
-    assert.doesNotThrow(() => patchbay.unlink());
+    patchbay = new venmic.PatchBay();
 }
 catch (error)
 {
@@ -24,4 +13,23 @@ catch (error)
 
     assert(!venmic.PatchBay.hasPipeWire());
     assert.throws(() => new venmic.PatchBay(), /(failed to create patchbay)|(not running pipewire)/ig);
+
+    process.exit(0);
 }
+
+assert(Array.isArray(patchbay.list()));
+assert(Array.isArray(patchbay.list(["node.name"])));
+
+assert.throws(() => patchbay.list({}), /expected list of strings/ig);
+assert.throws(() => patchbay.list([10]), /expected list of strings/ig);
+
+assert.throws(() => patchbay.link(10), /expected link object/ig);
+
+assert.throws(() => patchbay.link({ a: "A", b: "B", c: "C" }), /expected keys/ig);
+assert.throws(() => patchbay.link({ key: "node.name", value: "Firefox", mode: "gibberish" }), /expected keys/ig);
+
+assert.throws(() => patchbay.link({ props: [{ a: 0, b: 0 }], mode: "gibberish" }), /key-value/ig);
+assert.throws(() => patchbay.link({ props: [{ key: "", value: "" }], mode: "gibberish" }), /expected mode/ig);
+
+assert.doesNotThrow(() => patchbay.link({ props: [{ key: "node.name", value: "Firefox" }], mode: "include" }));
+assert.doesNotThrow(() => patchbay.unlink());
