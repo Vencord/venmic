1165b080fd5500b7b98f10e6b1b50e44282333f8
Curve <fynnbwdt@gmail.com>
2024-01-07 00:40:14 +0100

    refactor: update types

--- lib/module.d.ts
+++ lib/module.d.ts
@@ -5,18 +5,32 @@ type LiteralUnion<
 	BaseType extends string,
 > = LiteralType | (BaseType & Record<never, never>);
 
+type Optional<
+    Type, 
+    Key extends keyof Type
+> = Partial<Pick<Type, Key>> & Omit<Type, Key>;
+
 export interface Prop
 {
     key: string;
     value: string;
 }
 
+export interface LinkData
+{
+    include: Prop[];
+    exclude: Prop[];
+
+    ignore_devices?: boolean;
+}
+
+
 export class PatchBay
 {
     unlink(): void;
     
     list<T extends string = DefaultProps>(props?: T[]): Record<LiteralUnion<T, string>, string>[];
-    link(data: {include?: Prop[], exclude?: Prop[], ignore_devices?: boolean}): boolean;
+    link(data: Optional<LinkData, "exclude"> | Optional<LinkData, "include">): boolean;
 
     static hasPipeWire(): boolean;
 }
