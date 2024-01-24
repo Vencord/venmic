870f184aaddb85881f6d035101186c856c501b3a
Curve <fynnbwdt@gmail.com>
2024-01-24 22:34:22 +0100

    fix(addon): workaround is array

--- addon/addon.cpp
+++ addon/addon.cpp
@@ -162,7 +162,7 @@ struct patchbay : public Napi::ObjectWrap<patchbay>
         auto include        = to_array<vencord::prop>(data.Get("include"));
         auto exclude        = to_array<vencord::prop>(data.Get("exclude"));
         auto ignore_devices = convert<bool>(data.Get("ignore_devices"));
-        auto workaround     = convert<vencord::prop>(data.Get("workaround"));
+        auto workaround     = to_array<vencord::prop>(data.Get("workaround"));
 
         if (!include && !exclude)
         {
@@ -177,7 +177,7 @@ struct patchbay : public Napi::ObjectWrap<patchbay>
             .include        = include.value_or(std::vector<vencord::prop>{}),
             .exclude        = exclude.value_or(std::vector<vencord::prop>{}),
             .ignore_devices = ignore_devices.value_or(true),
-            .workaround     = workaround,
+            .workaround     = workaround.value_or(std::vector<vencord::prop>{}),
         });
 
         return Napi::Boolean::New(env, true);
