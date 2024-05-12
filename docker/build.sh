7eacac56dc6b2486ed48275903e043a66c4b7029
Curve <fynnbwdt@gmail.com>
2024-05-12 15:31:09 +0200

    refactor(ci): prefer ninja over make

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Ensure pnpm is loaded
 source /root/.bashrc
 
+# Let's get to work!
 cd /work
+
+# Export Threads for Make and Ninja
+export MAKEFLAGS=-j$(nproc)
+export PARALLEL_LEVEL=$(nproc)
+
 pnpm install --ignore-scripts && pnpm run install
