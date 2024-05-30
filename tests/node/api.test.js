1279b31a9656be675bb1b95bcccebd1d7f60eace
Curve <fynnbwdt@gmail.com>
2024-05-30 18:58:57 +0200

    refactor: update tests

--- tests/node/api.test.js
+++ tests/node/api.test.js
@@ -27,17 +27,17 @@ assert.throws(() => patchbay.link(10), /expected link object/ig);
 
 assert.throws(() => patchbay.link({ }), /'include' or 'exclude'/ig);
 assert.throws(() => patchbay.link({ a: "A", b: "B", c: "C" }), /'include' or 'exclude'/ig);
-assert.throws(() => patchbay.link({ key: "node.name", value: "Firefox", mode: "gibberish" }), /'include' or 'exclude'/ig);
+assert.throws(() => patchbay.link({ "node.name": "Firefox", mode: "gibberish" }), /'include' or 'exclude'/ig);
 
 assert.throws(() => patchbay.link({ include: "Firefox" }), /key-value/ig);
 assert.throws(() => patchbay.link({ include: {} }), /key-value/ig);
 
-assert.doesNotThrow(() => patchbay.link({ include: [{ key: "node.name", value: "Firefox" }] }));
-assert.doesNotThrow(() => patchbay.link({ exclude: [{ key: "node.name", value: "Firefox" }] }));
+assert.doesNotThrow(() => patchbay.link({ include: [{ "node.name": "Firefox" }] }));
+assert.doesNotThrow(() => patchbay.link({ exclude: [{ "node.name": "Firefox" }] }));
 
-assert.doesNotThrow(() => patchbay.link({ include: [{ key: "node.name", value: "Firefox" }], exclude: [{ key: "object.id", value: "100" }] }));
+assert.doesNotThrow(() => patchbay.link({ include: [{ "node.name": "Firefox" }], exclude: [{ "object.id": "100" }] }));
 
-assert.doesNotThrow(() => patchbay.link({ exclude: [{ key: "node.name", value: "Firefox" }], ignore_devices: true }));
-assert.doesNotThrow(() => patchbay.link({ exclude: [{ key: "node.name", value: "Firefox" }], ignore_devices: true, only_default_speakers: true }));
+assert.doesNotThrow(() => patchbay.link({ exclude: [{ "node.name": "Firefox" }], ignore_devices: true }));
+assert.doesNotThrow(() => patchbay.link({ exclude: [{ "node.name": "Firefox" }], ignore_devices: true, only_default_speakers: true }));
 
 assert.doesNotThrow(() => patchbay.unlink());
