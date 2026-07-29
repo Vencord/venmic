4d6584103a0888a66a0f4445df8d965b9a7a4771
Curve <fynnbwdt@gmail.com>
2026-07-29 14:42:24 +0200

    fix(alpine): add make

--- docker/musl/dependencies.sh
+++ docker/musl/dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apk add bash curl git tar nodejs npm libatomic
-apk add gcc g++ cmake ninja-build
+apk add gcc g++ make cmake ninja-build
 apk add pipewire pipewire-dev pipewire-libs pipewire-pulse pulseaudio-dev
 
 # Setup pnpm
